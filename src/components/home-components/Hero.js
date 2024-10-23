dbfeba0f8e78863853b22b4b2c52b85a04fae00b
Andres Romero <aromerosambucetti@gmail.com>
2024-10-23 03:07:02 -0300

    comienzo de las animaciones del hero

--- src/components/home-components/Hero.js
+++ src/components/home-components/Hero.js
@@ -14,7 +14,8 @@ const Hero = () => {
                 </div>
             </div>
             <div className="right-content">
-                <img className="logo" src="/images/iconos/LogoAF.png" alt="Logo" loading="lazy"/>
+                <img className="logo rotate-in-center" src="/images/iconos/LogoAF-empty.png" alt="Logo" loading="lazy"/>
+                <img className="logo-ball" src="/images/iconos/LogoAF-ball.png" alt="Logo-ball" loading="lazy"/>
             </div>
         </div>
     )
